bf37dbec1572c429fa26e5d2968c21b13c132597
SergeyBlinkov <blinok1896@gmail.com>
2024-03-06 20:25:25 +0400

    calculator project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './wrapper.css';
+import CalculatorIndex from './features/CalculatorDrag/CalculatorIndex';
+
+function App() {
+    return (
+        <CalculatorIndex />
+    );
+}
+
+export default App;
